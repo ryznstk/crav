4ab773764c96715fd3ec69e5d659f0e55e38cac5
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-06-21 07:34:28 +0700

    Update crave.sh

--- crave.sh
+++ crave.sh
@@ -7,7 +7,7 @@ set -e
 export PROJECTFOLDER="LOS"
 export PROJECTID="93"
 export REPO_INIT="https://github.com/accupara/los22.git -b lineage-22.1 --git-lfs --depth=1"
-export BUILD_DIFFERENT_ROM="repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs"
+export BUILD_DIFFERENT_ROM="repo init -u https://github.com/Lunaris-AOSP/android -b test --git-lfs"
 # ================================
 # Destroy Old Clones
 # ================================
