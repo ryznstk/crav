8cff5eba673f54d8ffbe763912c70b573c728ea1
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-07-28 04:28:50 +0700

    Update crave.sh

--- crave.sh
+++ crave.sh
@@ -58,6 +58,8 @@ crave run --no-patch -- "
   # Sync sources
   # ================================
 echo '>>> Syncing sources'
+/opt/crave/resync.sh
+
 
 if [ -f /opt/crave/resync.sh ]; then
     /opt/crave/resync.sh
